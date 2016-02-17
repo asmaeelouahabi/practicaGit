797f33f9df06fdf6550d05bc32bec3925925bfd3
loubna <rmhind@hotmail.com>
2016-02-17 15:02:30 +0100

    modificaciones asma

--- GitAux.java
+++ GitAux.java
@@ -1,6 +1,6 @@
 /*
  * To change this template, choose Tools | Templates
- 
+
  */
 package prgitejemplo;
 
@@ -9,18 +9,21 @@ package prgitejemplo;
  * @author bertoa
  */
 public class GitAux {
-    
+
     public void metodoAlumno1(){
         //System.out.println("metodo 1");
         System.out.println("metodo 1"); // añadir una linea
-         
+
     }
-    
+
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+    System.out.println("modificado por loubna");
     }
-    
+
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        //modificacion
+        System.out.println("loubna inserta una linea");
     }
 }
